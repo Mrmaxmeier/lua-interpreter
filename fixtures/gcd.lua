9941a2ff978eb8e7b8be0df9a7b1237f794d5fb0
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-08-16 19:16:33 +0200

    fix gcd, print gcd'd values

--- fixtures/gcd.lua
+++ fixtures/gcd.lua
@@ -2,14 +2,17 @@ function recursive_gcd(a, b)
   if b == 0 then
     return a
   else
-    return recursive_gcd.gcd(b, a % b)
+    return recursive_gcd(b, a % b)
   end
 end
 
 function gcd(m, n)
     while m ~= 0 do
-        m, n = math.mod(n, m), m
+        m, n = n % m, m
     end
 
     return n
 end
+
+print("recursive_gcd(99, 56) = " .. recursive_gcd(99, 56))
+print("gcd(123, 456) = " .. gcd(123, 456))
