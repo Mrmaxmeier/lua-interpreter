2a7db6362708abbf3ceafd3c977fff3dff1afae0
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-08-19 20:21:47 +0200

    add crappy shell script to compile lua files

---
+++ fixtures/build.sh
@@ -0,0 +1,15 @@
+compile() {
+    if [[ $1 == *.lua ]]; then
+        echo "compiling $1"
+        # TODO: handle files like "a.b.lua"
+        base=$(echo $1 | cut -d "." -f1)
+        cmd="./luac -o $base $1"
+        echo $cmd
+        $cmd
+        echo ""
+    fi
+}
+
+for file in $(ls .); do
+    compile $file
+done
