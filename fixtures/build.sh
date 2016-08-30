c897460297ebeaf225f493c1ea7ed328ddf47229
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-08-30 22:12:08 +0200

    add hello_world.lua fixture

--- fixtures/build.sh
+++ fixtures/build.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 compile() {
     if [[ $1 == *.lua ]]; then
         echo "compiling $1"
         # TODO: handle files like "a.b.lua"
         base=$(echo $1 | cut -d "." -f1)
-        cmd="./luac -o $base $1"
+        cmd="luac -o $base $1"
         echo $cmd
         $cmd
         echo ""
