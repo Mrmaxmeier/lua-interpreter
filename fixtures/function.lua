a26ab949590ea66b87532bafb8e397d931399876
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-09-20 15:49:33 +0200

    add tests to function.lua

--- fixtures/function.lua
+++ fixtures/function.lua
@@ -1,7 +1,14 @@
+local outside = 42
+
 function a()
+    local inside = 1337
     print("inside a")
+    return inside, 9
 end
 
 print("outside a")
-a()
+local from_inside, from_inside2 = a()
+assert(from_inside == 1337)
+assert(from_inside2 == 9)
+assert(outside == 42)
 print("after a")
